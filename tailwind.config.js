84e83f6fa36653453484f0b7bd021c6367f20aea
Tomas luquez <luquez1431@gmail.com>
2024-09-16 15:14:16 -0300

    config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#e3dace",
+        success: "#d2fbab",
+        "success-message": "#4b8c0c",
+        warning: "#f4f1b8",
+        "warning-message": "#fbbf3b",
+        error: "#fb9a87",
+        "error-message": "#f24726",
+      },
+    },
   },
   plugins: [],
 };
